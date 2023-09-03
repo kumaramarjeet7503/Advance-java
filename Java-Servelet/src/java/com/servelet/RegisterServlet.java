9293261ac059fd56b033a0fadef81850c088dca8
kumaramarjeet7503 <amarjeet1998@outlook.com>
2023-09-03 11:51:16 +0530

    Form post using servlet

---
+++ Java-Servelet/src/java/com/servelet/RegisterServlet.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.servelet;
+
+import javax.servlet.* ;
+import javax.servlet.http.* ;
+import java.io.IOException;
+import java.io.PrintWriter ;
+/**
+ *
+ * @author amarj
+ */
+public class RegisterServlet extends HttpServlet {
+    
+    @Override 
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException , IOException
+    {
+           response.setContentType("text/html") ;
+           PrintWriter out = response.getWriter() ;
+           out.println("<h3>Welcome to Register Servlet.</h3>");
+           
+           String name = request.getParameter("user-name");
+           String email = request.getParameter("user-email");
+           String password = request.getParameter("user-password");
+           String gender = request.getParameter("user-gender");
+           String course = request.getParameter("user-course");
+           String terms = request.getParameter("user-terms");
+           
+           if(terms != null){
+               out.println("<h5>Name : "+name+"</h5>");
+               out.println("<h5>email : "+email+"</h5>");
+               out.println("<h5>Password : "+password+"</h5>");
+               out.println("<h5>Gender : "+gender+"</h5>");
+               out.println("<h5>Course : "+course+"</h5>");
+               out.println("<h5>Terms : "+terms+"</h5>");
+           }else
+           {
+                out.println("<h3>Please accept terms and conditions first </h3>");
+           }
+           
+    }
+    
+}
