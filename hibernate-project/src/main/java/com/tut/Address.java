ee7f0cfb9e4516b0b67d5e8b94a2ebf1b2d8e73f
Amarjeet <kumaramarjeet001@gmail.com>
2023-09-28 22:46:46 +0530

    * Using basic annotations for data saving * Saving for Image via hibernate

---
+++ hibernate-project/src/main/java/com/tut/Address.java
@@ -0,0 +1,118 @@
+package com.tut;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.persistence.Transient;
+
+@Entity 
+@Table(name="address")
+public class Address {
+
+	@jakarta.persistence.Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column (name = "Id")
+	private int Id ;
+	
+	@Column (name="City",length=50)
+	private String City ;
+	@Column (name="StreetName",length=50)
+	private String Street ;
+	@Transient
+	private double Price ;
+	private int PostalCode ;
+	private String BoxNo ;
+	private boolean IsActive ;
+	
+	@Temporal(TemporalType.DATE) 
+	private Date CreatedDate ;
+	
+	@Lob 
+	private byte[] Image ;
+	
+	public Address() {
+		
+	}
+
+	public int getId() {
+		return Id;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+
+	public String getCity() {
+		return City;
+	}
+
+	public void setCity(String city) {
+		City = city;
+	}
+
+	public String getStreet() {
+		return Street;
+	}
+
+	public void setStreet(String street) {
+		Street = street;
+	}
+
+	public double getPrice() {
+		return Price;
+	}
+
+	public void setPrice(double price) {
+		Price = price;
+	}
+
+	public int getPostalCode() {
+		return PostalCode;
+	}
+
+	public void setPostalCode(int postalCode) {
+		PostalCode = postalCode;
+	}
+
+	public String getBoxNo() {
+		return BoxNo;
+	}
+
+	public void setBoxNo(String boxNo) {
+		BoxNo = boxNo;
+	}
+
+	public boolean isIsActive() {
+		return IsActive;
+	}
+
+	public void setIsActive(boolean isActive) {
+		IsActive = isActive;
+	}
+
+	public Date getCreatedDate() {
+		return CreatedDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		CreatedDate = createdDate;
+	}
+
+	public byte[] getImage() {
+		return Image;
+	}
+
+	public void setImage(byte[] image) {
+		this.Image = image;
+	}
+	
+	
+
+}
--- hibernate-project/src/main/java/com/tut/App.java
+++ hibernate-project/src/main/java/com/tut/App.java
@@ -1,5 +1,9 @@
 package com.tut;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Date;
+
 import org.hibernate.*;
 import org.hibernate.cfg.Configuration;
 
@@ -9,25 +13,40 @@ import org.hibernate.cfg.Configuration;
  */
 public class App 
 {
-	public static void main( String[] args )
+	public static void main( String[] args ) throws IOException
     {
     	Configuration cfg = new Configuration() ;
     	cfg.configure("hibernate.cfg.xml");
     	SessionFactory factory = cfg.buildSessionFactory();
 
         // Creating Student
-    	Student st = new Student() ;
-    	st.setId(1);
-    	st.setName("Amarjeet");
-    	st.setCity("Mohan Garden");
+//    	Student st = new Student() ;
+//    	st.setId(1);
+//    	st.setName("Amarjeet");
+//    	st.setCity("Mohan Garden");
 //    	
+    	
+//    	Saving for address
+    	Address address = new Address() ;
+    	address.setCity("Johanees berg") ;
+    	address.setStreet("Bolika") ;
+    	address.setPostalCode(12012) ;
+    	address.setBoxNo("3") ;
+    	address.setIsActive(true) ;
+    	address.setCreatedDate(new Date()) ;
+    	
+    	FileInputStream fis = new FileInputStream("src/main/java/alert.jpg") ;
+    	byte[] data = new byte[fis.available()] ;
+    	fis.read(data) ;
+    	address.setImage(data) ;
+    	
     	Session session = factory.openSession() ;
 //    	
     	Transaction tx = session.beginTransaction() ;
 
     	try 
     	{
-    		session.persist(st);
+    		session.persist(address);
         	tx.commit(); 
         	
     	}catch(Exception e) 
@@ -37,4 +56,7 @@ public class App
     	session.close(); 
     	
      }
+
+
+
 }
